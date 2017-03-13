204b7ef2eabb586e74e02714e3dc04cadbe92f2b
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-13 17:48:53 +0100

    date time done

--- widgets/date-time.js
+++ widgets/date-time.js
@@ -3,7 +3,21 @@
  */
 
 function dateTimeUpdate(){
-    $(".date-time").append("<p></p>").text(new Date().toDateString());
+
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var t = setTimeout(dateTimeUpdate, 1000);
+
+    $(".date-time").empty();
+
+    $("<h1></h1>").text(h + ":" + m + ":" + s).appendTo(".date-time");
+    $("<p></p>").text(today.toDateString()).appendTo(".date-time");
 }
-
-
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
