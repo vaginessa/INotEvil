63e89c41a3cd57c7f61772e90e570be04049714b
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-14 09:56:18 +0100

    youtube widget done

--- widgets/youtube.js
+++ widgets/youtube.js
@@ -5,19 +5,30 @@
 function youtubeUpdate(){
 
     $("#youtube").empty();
-
     var search = $("<input/>").attr("type","text").attr("id","youtube-video-search");
-    var button = $("<button></button>").attr("type","button").attr("onclick", "videoSearch();").text("Chercher");
+
+    var button = $("<button></button>").attr("type","button").attr("onclick", "newVideo();").text("Chercher");
 
     var form = $("<form></form>").append(search).append(button);
 
     form.appendTo("#youtube");
+
+    $("<iframe />", {
+        id : "youtube-player",
+        src : "http://www.youtube.com/embed?listType=search&list=tchoin",
+        type : "text/html",
+        width : 600,
+        height : 400,
+        frameborder : 0
+    }).appendTo("#youtube");
 }
 
-function videoSearch(){
+function newVideo(){
 
     var search = $("#youtube-video-search").val();
+
     $("#youtube-video-search").val("");
 
-    //TODO
+    $("#youtube-player")
+        .attr("src","http://www.youtube.com/embed?listType=search&list=" + search);
 }
