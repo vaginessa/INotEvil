5d68217873478d6656deca38ed21d9e3f562d056
ShellCode33 <clement.fleury33@gmail.com>
2017-03-18 17:24:30 +0100

    Dock création de widgets

--- js/widgets_manager.js
+++ js/widgets_manager.js
@@ -2,9 +2,21 @@
  * Created by shellcode on 3/14/17.
  */
 
-function manage() {
+Widgets = {
+    CLOCK : 0,
+    WEATHER : 1,
+    YOUTUBE : 2,
+    TWITTER : 3,
+    FOOTBALL : 4,
+    FLICKR : 5,
+    MAPS : 6
+};
 
-    update();
+//int array qui contient le nombre de widget qu'il y a sur chaque colonne pour que lors de l'ajout d'un nouveau widget,
+//celui-ci soit ajouté à la colonne avec le moins de widgets
+var widgets_count_per_column = [0, 0, 0];
+
+function manage() {
 
     $(".sortable").sortable({
         items: '.panel:not(.unsortable)',
@@ -46,11 +58,66 @@ function manage() {
     });
 }
 
-function update() {
-    clockUpdate();
-    weatherUpdate();
-    youtubeUpdate();
-    twitterUpdate();
-    footballUpdate();
-    picsUpdate();
+function addWidget(widgetType) {
+
+    var column_destination = 0;
+
+    //On prend la colonne avec le moins de widgets
+    $.each(widgets_count_per_column, function(i, val) {
+        if(val < widgets_count_per_column[column_destination])
+            column_destination = i;
+    });
+
+
+    var panel_widget = $("<div></div>").attr("class", "panel panel-primary panel-custom")
+                                                            .append('<div class="panel-heading unsortable"></div>')
+                                                            .append('<div class="widget panel-body unsortable"></div>');
+
+    $("#column" + (column_destination+1)).append(panel_widget);
+
+    switch (widgetType) {
+        case Widgets.CLOCK:
+            panel_widget.children(".panel-heading").text("Clock");
+            panel_widget.children(".widget").attr("id", "clock");
+            clockUpdate();
+            break;
+
+        case Widgets.WEATHER:
+            panel_widget.children(".panel-heading").text("Weather");
+            panel_widget.children(".widget").attr("id", "weather");
+            weatherUpdate();
+            break;
+
+        case Widgets.YOUTUBE:
+            panel_widget.children(".panel-heading").text("Youtube");
+            panel_widget.children(".widget").attr("id", "youtube");
+            youtubeUpdate();
+            break;
+
+        case Widgets.TWITTER:
+            panel_widget.children(".panel-heading").text("Twitter");
+            panel_widget.children(".widget").attr("id", "twitter");
+            twitterUpdate();
+            break;
+
+        case Widgets.FOOTBALL:
+            panel_widget.children(".panel-heading").text("Football");
+            panel_widget.children(".widget").attr("id", "football");
+            footballUpdate();
+            break;
+
+        case Widgets.FLICKR:
+            panel_widget.children(".panel-heading").text("Flickr");
+            panel_widget.children(".widget").attr("id", "pics");
+            picsUpdate();
+            break;
+
+        case Widgets.MAPS:
+            panel_widget.children(".panel-heading").text("Map");
+            panel_widget.children(".widget").attr("id", "maps").append('<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBn1D5NzOcdVR1r4PBqLYKJh5Gb6zdb9DA&callback=mapsUpdate"></script>');
+            break;
+    }
+
+    //On met à jour le tableau du nombre de widgets par colonne
+    widgets_count_per_column[column_destination]++;
 }
