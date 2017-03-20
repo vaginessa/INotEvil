d7f6adff27e4bf61b637f643407e528d3b64356d
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-20 14:39:19 +0100

    du sale

--- js/widgets_manager.js
+++ js/widgets_manager.js
@@ -3,27 +3,28 @@
  */
 
 Widgets = {
-    CLOCK : 0,
-    WEATHER : 1,
-    YOUTUBE : 2,
-    TWITTER : 3,
-    FOOTBALL : 4,
-    FLICKR : 5,
-    MAPS : 6
+    CLOCK: 0,
+    WEATHER: 1,
+    YOUTUBE: 2,
+    TWITTER: 3,
+    FOOTBALL: 4,
+    FLICKR: 5,
+    MAPS: 6
 };
 
 function manage() {
 
     $(".sortable").sortable({
         items: '.panel:not(.unsortable)',
+        handle : '.panel-heading',
         revert: true,
         dropOnEmpty: true,
         connectWith: "div.col_custom",
-        stop: function(event, ui) {
+        stop: function (event, ui) {
             var twitter = ui.item.children("#twitter");
 
             //Si twitter a été trouvé, c'est qu'il a été drag&drop et donc on le reload (pour fix le bug de la page blanche)
-            if(twitter.length > 0) {
+            if (twitter.length > 0) {
                 console.log("Reload twitter");
                 reload_twitter();
             }
@@ -59,59 +60,57 @@ function addWidget(widgetType) {
     var column_destination = 1; //colonnes 1, 2, ou 3
 
     //On prend la colonne avec le moins de widgets
-    if($("#column1").children().length > $("#column2").children().length)
+    if ($("#column1").children().length > $("#column2").children().length)
         column_destination = 2;
 
-    if($("#column" + column_destination).children().length > $("#column3").children().length)
+    if ($("#column" + column_destination).children().length > $("#column3").children().length)
         column_destination = 3;
 
-    var panel_widget = $("<div></div>") .attr("class", "panel panel-primary panel-custom")
-                                        .append('<div class="panel-heading unsortable"></div>')
-                                        .append('<div class="widget panel-body unsortable"></div>');
+    $("#column" + column_destination).load("js/widget/layout/panel_widget.html", function () {
 
-    $("#column" + column_destination).append(panel_widget);
+        switch (widgetType) {
+            case Widgets.CLOCK:
+                $(this).children().children(".panel-heading").children(".title").text("Clock");
+                $(this).children().children(".widget").attr("id", "clock");
+                clockUpdate();
+                break;
 
-    switch (widgetType) {
-        case Widgets.CLOCK:
-            panel_widget.children(".panel-heading").text("Clock");
-            panel_widget.children(".widget").attr("id", "clock");
-            clockUpdate();
-            break;
+            case Widgets.WEATHER:
+                $(this).children().children(".panel-heading").children(".title").text("Weather");
+                $(this).children().children(".widget").attr("id", "weather");
+                weatherUpdate();
+                break;
 
-        case Widgets.WEATHER:
-            panel_widget.children(".panel-heading").text("Weather");
-            panel_widget.children(".widget").attr("id", "weather");
-            weatherUpdate();
-            break;
+            case Widgets.YOUTUBE:
+                $(this).children().children(".panel-heading").children(".title").text("Youtube");
+                $(this).children().children(".widget").attr("id", "youtube");
+                youtubeUpdate();
+                break;
 
-        case Widgets.YOUTUBE:
-            panel_widget.children(".panel-heading").text("Youtube");
-            panel_widget.children(".widget").attr("id", "youtube");
-            youtubeUpdate();
-            break;
+            case Widgets.TWITTER:
+                $(this).children().children(".panel-heading").children(".title").text("Twitter");
+                $(this).children().children(".widget").attr("id", "twitter");
+                twitterUpdate();
+                break;
 
-        case Widgets.TWITTER:
-            panel_widget.children(".panel-heading").text("Twitter");
-            panel_widget.children(".widget").attr("id", "twitter");
-            twitterUpdate();
-            break;
+            case Widgets.FOOTBALL:
+                $(this).children().children(".panel-heading").children(".title").text("Football");
+                $(this).children().children(".widget").attr("id", "football");
+                footballUpdate();
+                break;
 
-        case Widgets.FOOTBALL:
-            panel_widget.children(".panel-heading").text("Football");
-            panel_widget.children(".widget").attr("id", "football");
-            footballUpdate();
-            break;
+            case Widgets.FLICKR:
+                $(this).children().children(".panel-heading").children(".title").text("Flickr");
+                $(this).children().children(".widget").attr("id", "pics");
+                picsUpdate();
+                break;
 
-        case Widgets.FLICKR:
-            panel_widget.children(".panel-heading").text("Flickr");
-            panel_widget.children(".widget").attr("id", "pics");
-            picsUpdate();
-            break;
-
-        case Widgets.MAPS:
-            panel_widget.children(".panel-heading").text("Map");
-            panel_widget.children(".widget").attr("id", "maps");
-            mapsUpdate();
-            break;
-    }
+            case Widgets.MAPS:
+                $(this).children().children(".panel-heading").children(".title").text("Map");
+                $(this).children().children(".widget").attr("id", "maps");
+                mapsUpdate();
+                break;
+        }
+    });
 }
+
