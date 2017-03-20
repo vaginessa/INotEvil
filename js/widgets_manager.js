9e103543c788df5e18be7274c7898a8e4031079b
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-20 16:57:49 +0100

    reduce and remove widget done

--- js/widgets_manager.js
+++ js/widgets_manager.js
@@ -79,10 +79,12 @@ function addWidget(widgetType) {
         });
 
         switch (widgetType) {
+
             case Widgets.CLOCK:
                 created_widgets_count["clock"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Clock");
                 $(this).children().children(".widget").attr("id", "clock" + created_widgets_count["clock"]);
+                $(this).find(".reduce").attr("data-target", "#clock" + created_widgets_count["clock"]);
                 clockUpdate(created_widgets_count["clock"]);
                 break;
 
@@ -90,6 +92,7 @@ function addWidget(widgetType) {
                 created_widgets_count["weather"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Weather");
                 $(this).children().children(".widget").attr("id", "weather" + created_widgets_count["weather"]);
+                $(this).find(".reduce").attr("data-target", "#weather" + created_widgets_count["weather"]);
                 weatherUpdate(created_widgets_count["weather"]);
                 break;
 
@@ -97,6 +100,7 @@ function addWidget(widgetType) {
                 created_widgets_count["youtube"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Youtube");
                 $(this).children().children(".widget").attr("id", "youtube" + created_widgets_count["youtube"]);
+                $(this).find(".reduce").attr("data-target", "#youtube" + created_widgets_count["youtube"]);
                 youtubeUpdate(created_widgets_count["youtube"]);
                 break;
 
@@ -104,6 +108,7 @@ function addWidget(widgetType) {
                 created_widgets_count["twitter"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Twitter");
                 $(this).children().children(".widget").attr("id", "twitter" + created_widgets_count["twitter"]);
+                $(this).find(".reduce").attr("data-target", "#twitter" + created_widgets_count["twitter"]);
                 twitterUpdate(created_widgets_count["twitter"]);
                 break;
 
@@ -111,6 +116,7 @@ function addWidget(widgetType) {
                 created_widgets_count["football"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Football");
                 $(this).children().children(".widget").attr("id", "football" + created_widgets_count["football"]);
+                $(this).find(".reduce").attr("data-target", "#football" + created_widgets_count["football"]);
                 footballUpdate(created_widgets_count["football"]);
                 break;
 
@@ -118,6 +124,7 @@ function addWidget(widgetType) {
                 created_widgets_count["flickr"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Flickr");
                 $(this).children().children(".widget").attr("id", "pics" + created_widgets_count["flickr"]);
+                $(this).find(".reduce").attr("data-target", "#pics" + created_widgets_count["flickr"]);
                 picsUpdate(created_widgets_count["flickr"]);
                 break;
 
@@ -125,11 +132,18 @@ function addWidget(widgetType) {
                 created_widgets_count["maps"]++;
                 $(this).children().children(".panel-heading").children(".title").text("Map");
                 $(this).children().children(".widget").attr("id", "maps" + created_widgets_count["maps"]);
+                $(this).find(".reduce").attr("data-target", "#maps" + created_widgets_count["maps"]);
                 mapsUpdate(created_widgets_count["maps"]);
                 break;
+
         }
+        $(this).find(".remove").attr("onclick", 'removeWidget("' + $(this).find(".widget").attr("id") + '");');
     });
 
     $("#column" + column_destination).append(panel_widget);
+}
+
+function removeWidget(id) {
+    $("#" + id).parentsUntil(".ui-resizable").remove();
 }
 
